fb3900fe4ffac19b66276f725f2caf46f3e3e3fd
Vidyakjain <karkalavidyashree@gmail.com>
2024-05-20 00:31:36 +0530

    Python variables

--- Vidyakjain/python variables.py
+++ Vidyakjain/python variables.py
@@ -3,6 +3,15 @@ print(var1)
 print(type(var1))
 print(id(var1))
 
-var2="Good mornning"
+var2="Good morning"
 print(type(var2))
 print(id(var2))
+print("_" *50)
+
+##(a+b)^2=a^2+b^2+2ab
+a=2
+b=3
+LHS=(a+b)**2
+print("LHS is:",LHS)
+RHS=a**2+b**2+2*a*b
+print("RHS is:",RHS)
