3596fb4b79c67c39dad6c4576c8616c7638af1bf
praveenamishra.p <praveenamishra698@gmail.com>
2024-05-28 21:47:14 +0530

    if else p1

---
+++ Praveena/ifelseprograms.py
@@ -0,0 +1,11 @@
+# divisble by 3
+
+# Take input
+i = int(input("Please enter number :"))
+# Check whether the number is divisible by 3
+if i%3 == 0:
+    print("Number is divisible by 3")
+else:
+    print("Number is not divisible by 3")
+
+
