bdad43a2622d9572843192080f5706599e7ae9e7
chetandgithub <chetan.dhok@gmail.com>
2024-05-29 20:32:25 +0530

    Python loop session....

--- Chetand/Seleniumpythonprogramming/Python_Loop_condition/Python_loop_session.py
+++ Chetand/Seleniumpythonprogramming/Python_Loop_condition/Python_loop_session.py
@@ -25,3 +25,10 @@ print("@" * 50)
 for i in range(1, 20):
     if i % 2 == 0:
         print(i)
+
+# 5th Example loops program to find those numbers which are divisible by 7 and multiple of 5, between 800 and 1762#
+
+for i in range(800, 1762):
+    if i % 6 == 0 and i % 4 == 0:
+        print(i)
+
