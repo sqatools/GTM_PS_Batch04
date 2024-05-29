94e956554cfeb5259ed4bd90552bb9356b9f8b4b
Neha2894 <nimjeneha2894@gmail.com>
2024-05-29 18:30:41 +0530

    Recreated If Else Examples file

--- Neha/PythonIfElseConditions/PythonIfElseExamples.py
+++ Neha/PythonIfElseConditions/PythonIfElseExamples.py
@@ -4,14 +4,17 @@ if a%3 ==0:
    print("a is divisible by 3")
 else:
     print("a is not divisible by 3")
+
 #Python program to check whether the given number is negative or not.
 a = -45
 if a<0:
     print("Number is negative")
 else:
     print("Number is positive")
+
 #Using a python program take input from the user between 1 to 7 and
 # print the day according to the number. 1 for Sunday 2 for Monday so on.
+
 x = int(input("Enter a number: "))
 if x == 1:
     print("Sunday")
@@ -48,9 +51,6 @@ elif operation == "/":
 else:
     print("Invalid operation")
 
-
-
-
 #Python program to check whether the citizen is a senior citizen or not.
 #An age greater than 60 than the given citizen is a senior citizen.
 
@@ -59,6 +59,7 @@ if age>60:
     print("Citizen is senior citizen")
 else:
     print("Citizen is not senior citizen")
+
 #Python program to print the largest number from two numbers.
 a = 215
 b = 45
