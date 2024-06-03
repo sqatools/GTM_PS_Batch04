72572bd24686bb81fde8969fb1422e5fc6785a32
mahilagithub <mahila618mahesh@gmail.com>
2024-06-03 10:15:35 +0530

    Home Exercise2

---
+++ Mahila/pythonprogramming/not solved.py
@@ -0,0 +1,4 @@
+#Python program to check whether the given number is an Armstrong number or not.
+#Python program to print the current date in the given format
+#Python program to calculate days between 2 dates.
+           #Input date : (2023, 1, 5) (2023, 1, 22)
