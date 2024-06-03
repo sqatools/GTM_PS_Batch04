dce099f6918cbb89f7a9bce25297e8b73711bf65
mahilagithub <mahila618mahesh@gmail.com>
2024-06-03 10:23:07 +0530

    Home Exercise

--- Mahila/pythonprogramming/not solved.py
+++ Mahila/pythonprogramming/not solved.py
@@ -2,3 +2,6 @@
 #Python program to print the current date in the given format
 #Python program to calculate days between 2 dates.
            #Input date : (2023, 1, 5) (2023, 1, 22)
+#Program to get the factorial of the given number.
+#Program to get the Fibonacci series between 0 to 50.
+#Python program to check given number is palindrome or not.
--- Mahila/pythonprogramming/pythonvariables/pythonvariablefirstfile.py
+++ Mahila/pythonprogramming/pythonvariables/pythonvariablefirstfile.py
@@ -248,3 +248,5 @@ Python program to calculate the area of the square.
 Formula : area = a*a
 """
 
+#Python program to calculate compound interest
+
