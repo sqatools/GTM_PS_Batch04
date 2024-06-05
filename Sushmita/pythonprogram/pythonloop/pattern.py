e3e2556ee2c1ef2072fcbfcd0aec9b47e977648a
Sushmitashastri23 <shastrisushmita23@gmail.com>
2024-06-05 21:07:40 +0530

    new string file added

--- Sushmita/pythonprogram/pythonloop/pattern.py
+++ Sushmita/pythonprogram/pythonloop/pattern.py
@@ -1,3 +1,39 @@
-for i in range (0,6):
-    for j in range()
-print(dict(str))
+'''
+ * * *
+*     *
+*     *
+*     *
+*     *
+ * * *
+'''
+
+
+
+for i in range (1):
+    for j in range(5):
+        if i==0:
+            if j==0 or j== 4:
+                print(' ',end=' ')
+            else:
+                print('*',end=' ')
+
+    print( )
+
+for i in range(4):
+    for j in range(5):
+        if j==1 or j==2 or j==3:
+            print(' ',end=' ')
+        else:
+            print('*',end=' ')
+    print( )
+
+for i in range(1):
+    for j in range(5):
+        if j==0 or j==4:
+            print(' ',end=' ')
+        else:
+            print('*',end=' ')
+    print( )
+
+
+
