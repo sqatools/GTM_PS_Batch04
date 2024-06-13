e1cf69c535a1ed794948af9bf0a2e31365d0f50b
Vidyakjain <karkalavidyashree@gmail.com>
2024-06-13 23:43:35 +0530

    basic pgm

--- Vidyakjain/Python basic programming.py
+++ Vidyakjain/Python basic programming.py
@@ -73,6 +73,33 @@ d=v**2+i**2
 print("Hypoteneous id",math.sqrt(d))
 
 
+########Python program to solve the given math formula########
+##########Formula : (a + b)2 = a^2 + b^2 + 2ab#########
+a=2
+b=3
+d=a**2+b**2+2*a*b
+print("result is:",d)
+
+
+##########Python program to solve the given math formula.###########
+############(a – b)2 = a^2 + b^2 – 2ab
+
+a=4
+b=5
+d=a**2+b**2-(2*a*b)
+print("result is:",d)
+
+##########Python program to solve the given math formula.######
+#######a^2 – b^2 = (a-b)(a+b)#############
+
+a=4
+b=3
+d=(a-b)*(a+b)
+print("result is:",d)
+
+
+
+
 
 
 
