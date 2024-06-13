3188c734ce7c7214e3aaf8c6bea3e858caa48945
Vidyakjain <karkalavidyashree@gmail.com>
2024-06-13 22:40:26 +0530

    basic pgm

---
+++ Vidyakjain/Python basic programming.py
@@ -0,0 +1,43 @@
+#####Program to add two integers############
+a=2
+b=3
+print(a+b)
+
+
+############Subtract 2 integers########
+a=3
+b=4
+print(a-b)
+
+##########Multip;y 2 integers###
+a=4
+b=4
+print(a*b)
+
+#########pgm to repeat a given string 5 times######
+str1="Vidya is learning python"
+print(str1*5)
+
+##########pgm to get the avg of given numbers######
+a=3
+b=4
+c=5
+d=a+b+c
+print("Avg of 3 numbers:",d/3)
+
+##########pgm to get the median of given number#####
+
+list1=[2,3,4,5]
+list1.sort()
+a=(len(list1))/2
+print("Median is:",list1[int(a)])
+
+
+###########print a square and cube of given number########
+
+a=2
+b=3
+print("Square of 2:",a**2)
+print("Cube of 3:",b**3)
+
+
