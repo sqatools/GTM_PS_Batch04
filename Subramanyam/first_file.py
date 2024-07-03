4dab73bae4eb0e6d21207a21cf46fec689cd0fe7
Subra133 <subramanyamdasini@gmail.com>
2024-07-03 20:57:58 +0530

    filehandling1 practice

--- Subramanyam/first_file.py
+++ Subramanyam/first_file.py
@@ -1,1 +1,11 @@
-print("hello Subbu")
+names = ['Alice', 'Bob', 'Charlie']
+scores = [85, 92, 78]
+
+student_details=[(name,score) for name,score in zip(names,scores)]
+print(student_details)
+
+def read_file_byte_data(filename,num_byte):
+    with open(filename,"r") as file:
+        data=file.read(num_byte)
+        print(data)
+read_file_byte_data("first_file_py001.txt",110)
