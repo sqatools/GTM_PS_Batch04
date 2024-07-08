31d4872a62cdd8154d0eda8029733d55559b9768
GitKirti <kirtilabana5@gmail.com>
2024-07-08 22:15:43 +0530

    Test practice

---
+++ Kirti/ExcelfileHW.py
@@ -0,0 +1,17 @@
+#
+# #Q1 write a python program to enter all even number from 1 to 20 in A colom
+#
+# import openpyxl
+#
+# #######################
+# # update the exel sheet data
+#
+# def update_excel_sheet_data(filename, sheet_name, cell_name, new_data):
+#     wb = openpyxl.load_workbook(filename)
+#     sheet = wb[sheet_name]
+#     sheet[cell_name] = new_data
+#     wb.save(filename)
+#
+# #update_excel_sheet_data("test_data.xlsx", "Sheet1", "A1", "Chennai")
+
+
