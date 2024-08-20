4c0b48283177266fa878d9bf858b38045de8ee40
keerthilatha10 <keerthiymglobal@gmail.com>
2024-08-19 23:18:00 -0400

    commit

--- keerthi/pythonselenium/selenium basic/Advance xpath methods.py
+++
@@ -1,4 +0,0 @@
-# i) following: # All element below chintu
-
-# //body[@class="Desktop"]//following:p[text="click to add a return flight"]
-//span[text()='From']//following::p[text() = 'Enter city or airport']
