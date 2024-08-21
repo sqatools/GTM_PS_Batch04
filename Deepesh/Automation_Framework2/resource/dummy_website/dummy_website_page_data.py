217b6a77ab901de7b9c33d44f5afbc8c29fc5b01
Bi12Python <xovek11875@bitvoo.com>
2024-08-21 22:14:13 +0530

    Read data from json file

--- Deepesh/Automation_Framework2/resource/dummy_website/dummy_website_page_data.py
+++ Deepesh/Automation_Framework2/resource/dummy_website/dummy_website_page_data.py
@@ -6,5 +6,12 @@ first_name_value = "John"
 last_name_value = "Jenny"
 
 #### locators ######
-first_name_locator = (By.XPATH, "(//input[@name='firstname1'])[1]")
+first_name_locator = (By.XPATH, "(//input[@name='firstname'])[1]")
 last_name_locator = (By.XPATH, "(//input[@name='firstname'])[2]")
+
+dob_locator = (By.ID, "birthday")
+male_locator = (By.ID, "male")
+addmore_passenger_dd_locator = (By.ID, "admorepass")
+one_way_locator = (By.ID, "oneway")
+from_city_locator =(By.ID, "fromcity")
+dest_city_locator = (By.ID, "destcity")
