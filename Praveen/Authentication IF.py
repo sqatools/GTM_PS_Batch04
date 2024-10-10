a191f5eea8fa82f60f4603bdfa8c44d118008529
Praveen0923 <praveenavula19@gmail.com>
2024-10-10 12:12:02 +0530

    Authentication

---
+++ Praveen/Authentication IF.py
@@ -0,0 +1,7 @@
+username = "Praveen"
+Password = "praveen@hai"
+
+if username == "Praveen" and Password == "praveen@hai":
+    print ("Authrentication Succesfull")
+else :
+    print("Worng Username and password ")
